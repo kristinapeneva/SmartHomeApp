024b5a1fd1935cab92877580ee9b802d87120368
Kristina <cristina.peneva@gmail.com>
2021-10-25 22:00:16 +0200

    basics - Login/SignUp

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+
 }
 
 const styles = StyleSheet.create({
---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/auth"
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const app = firebase.initializeApp ({
+  apiKey: "AIzaSyD-yopmPNi6Hr9lJyRiWcV1XHMgmEi2GPU",
+  authDomain: "smarthomeapp-52e02.firebaseapp.com",
+  projectId: "smarthomeapp-52e02",
+  storageBucket: "smarthomeapp-52e02.appspot.com",
+  messagingSenderId: "849217606595",
+  appId: "1:849217606595:web:ef83eaf9a26e42fdd46376",
+  measurementId: "G-63B5QEVBG7"
+})
+
+// Initialize Firebase
+
+export const auth = app.auth()
+export default app
